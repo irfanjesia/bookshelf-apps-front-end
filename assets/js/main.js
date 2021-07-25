059a63928c8511d45faf87e1018512899efd647a
irfanjesia <irfanjesia31@gmail.com>
2021-07-25 14:42:51 +0700

    reformatting js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,16 +1,16 @@
-window.addEventListener("load", (function() {
-    if (typeof(Storage) !== "undefined") {
-        if (localStorage.getItem(localStorageKey) === null) {
-            localStorage.setItem(localStorageKey, null);
-        }
-    } else {
-        alert("Browser yang Anda gunakan tidak mendukung Web Storage");
+window.addEventListener ('load', function () {
+  if (typeof Storage !== 'undefined') {
+    if (localStorage.getItem (localStorageKey) === null) {
+      localStorage.setItem (localStorageKey, null);
     }
+  } else {
+    alert ('Browser yang Anda gunakan tidak mendukung Web Storage');
+  }
 
-    books = JSON.parse(localStorage.getItem(localStorageKey)) || books;
-    showBook(books);
+  books = JSON.parse (localStorage.getItem (localStorageKey)) || books;
+  showBook (books);
 
-    formInput.addEventListener("submit", addBook);
-    formSearch.addEventListener("submit", findBook);
-    document.addEventListener("newBook", makeBook);
-}))
+  formInput.addEventListener ('submit', addBook);
+  formSearch.addEventListener ('submit', findBook);
+  document.addEventListener ('newBook', makeBook);
+});
